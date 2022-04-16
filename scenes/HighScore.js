5f91bb14eec55984732d06533289f039093855eb
freakpants <freakpants@gmail.com>
2022-04-16 11:44:59 +0200

    ladder selection

--- scenes/HighScore.js
+++ scenes/HighScore.js
@@ -36,223 +36,269 @@ export default class HighScore extends FindItScene {
         " Players";
       const levelversion =
         document.highScoreRounds + "-" + document.highScorePlayers;
-      air_console.requestHighScores(levelname, levelversion);
+      if (this.table !== undefined) {
+        this.table.destroy();
+      }
+      air_console.requestHighScores(levelname, levelversion, uids, [
+        document.highScoreRegion,
+      ]);
       document.highScoreSettingChanged = false;
       document.scores = false;
     }
     if (document.scores && this.scoreBuildingInProgress == false) {
-      this.scoreBuildingInProgress = true;
-      // conventionally sort the scores
-      document.scores.sort(function (a, b) {
-        if (a.ranks[document.highScoreRegion] < b.ranks[document.highScoreRegion]) {
-          return -1;
-        }
-        if (a.ranks[document.highScoreRegion] > b.ranks[document.highScoreRegion]) {
-          return 1;
-        }
-        return 0;
-      });
-
-      // cache profile pictures
-      let loader = new Phaser.Loader.LoaderPlugin(this);
-      var requested = [];
-      document.scores.forEach((score) => {
-        const uids = score.uids;
-        uids.forEach((uid) => {
-          const image_id = "user_" + uid;
-          DEBUG && console.log("requested image array:");
-          DEBUG && console.log(requested);
-          if (!requested.includes(image_id)) {
-            loader.image(
-              image_id,
-              air_console.getProfilePicture(
-                uid,
-                Math.floor(110 * document.uiScale)
-              )
-            );
-            requested.push(image_id);
+      if (document.scores.length === 0) {
+        this.scoreText.setText("No Highscores for the selected settings");
+      } else {
+        this.scoreBuildingInProgress = true;
+        // conventionally sort the scores
+        document.scores.sort(function (a, b) {
+          if (
+            a.ranks[document.highScoreRegion] <
+            b.ranks[document.highScoreRegion]
+          ) {
+            return -1;
           }
+          if (
+            a.ranks[document.highScoreRegion] >
+            b.ranks[document.highScoreRegion]
+          ) {
+            return 1;
+          }
+          return 0;
         });
-      });
 
-      loader.once(Phaser.Loader.Events.COMPLETE, () => {
-        // attempt to draw the high score table
-        this.scoreText.setText("HighScores");
-        this.regionText.setText(
-          document.highScoreRegion +
-            " - " +
-            document.highScorePlayers +
-            " Player" +
-            (document.highScorePlayers > 1 ? "s" : "") +
-            " - " +
-            document.highScoreRounds +
-            " Rounds"
-        );
+        // cache profile pictures
+        let loader = new Phaser.Loader.LoaderPlugin(this);
+        var requested = [];
+        document.scores.forEach((score) => {
+          const uids = score.uids;
+          uids.forEach((uid) => {
+            const image_id = "user_" + uid;
+            DEBUG && console.log("requested image array:");
+            DEBUG && console.log(requested);
+            if (!requested.includes(image_id)) {
+              loader.image(
+                image_id,
+                air_console.getProfilePicture(
+                  uid,
+                  Math.floor(110 * document.uiScale)
+                )
+              );
+              requested.push(image_id);
+            }
+          });
+        });
 
-        const cellwidth = 200 * uiScale;
-        const cellheight = 64 * uiScale;
-        const columnCount = 5;
-        const rowCount = document.scores.length + 1;
-
-        const cellsCount = columnCount * rowCount;
-        const gridwidth = columnCount * cellwidth;
-        const gridheight = rowCount * cellheight;
-
-        document.graphics = this.graphics;
-
-        document.wideCells = [];
-
-        var newCellObject = function (scene, cell) {
-          let cellText = "";
-          let bgColor;
-          let odd = false;
-          let firstCellofRow = 0;
-
-          // determine first cell of row, then determine if its an odd or even row based on that
-          const remainder = cell.index % columnCount;
-          if (remainder === 0) {
-            firstCellofRow = cell.index;
-          } else {
-            firstCellofRow = cell.index - remainder;
+        loader.once(Phaser.Loader.Events.COMPLETE, () => {
+          // attempt to draw the high score table
+          let highScoreRegion = document.highScoreRegion;
+          if(document.highScoreRegion === "city" && document.scores.length > 0 && document.scores[0].location_city_name !== undefined) {
+            highScoreRegion = document.scores[0].location_city_name;
           }
-          const rowNumber = firstCellofRow / columnCount;
-          if (rowNumber % 2 === 0) {
-            // odd
-            odd = true;
+          if(document.highScoreRegion === "country" && document.scores.length > 0 && document.scores[0].location_country_name !== undefined) {
+            highScoreRegion = document.scores[0].location_country_name;
           }
-
-          DEBUG && console.log("row number:");
-          console.log(rowNumber);
-
-          // determine if this is the head row
-          if (cell.index < columnCount) {
-            bgColor = Phaser.Display.Color.HexStringToColor("#002171").color;
-          } else if (odd) {
-            bgColor = Phaser.Display.Color.HexStringToColor("#0d47a1").color;
-          } else {
-            bgColor = Phaser.Display.Color.HexStringToColor("#2962ff").color;
+          if(document.highScoreRegion === "region" && document.scores.length > 0 && document.scores[0].location_region_name !== undefined) {
+            highScoreRegion = document.scores[0].location_region_name;
           }
-
-          var cellBg = scene.add
-            .graphics(0, 0)
-            .fillStyle(bgColor)
-            .fillRect(0, 0, 200 * uiScale, 64 * uiScale);
-
-          switch (cell.index) {
-            case 0:
-              cellText = "Position";
-              break;
-            case 1:
-              cellText = "Avatar";
-              break;
-            case 2:
-              cellText = "Nickname";
-              document.wideCells.push(cell.index);
-              break;
-            case 3:
-              cellText = "Time";
-              break;
-            case 4:
-              cellText = "Score";
-              break;
-            default:
-              cellText = cell.index;
-              break;
+          this.scoreText.setText("HighScores");
+          if(document.highScoreRegion === "world" || document.highScoreRegion === "friends") {
+            this.regionText.setText(
+              highScoreRegion +
+                "\n" +
+                document.highScorePlayers +
+                " Player" +
+                (document.highScorePlayers > 1 ? "s" : "") +
+                "\n" +
+                document.highScoreRounds +
+                " Rounds"
+            );
+          }else {
+            this.regionText.setText(
+              document.highScoreRegion + ": " +
+              highScoreRegion +
+                "\n" +
+                document.highScorePlayers +
+                " Player" +
+                (document.highScorePlayers > 1 ? "s" : "") +
+                "\n" +
+                document.highScoreRounds +
+                " Rounds"
+            );
           }
 
-          var image = null;
-          // dont do this in head row
-          if (rowNumber > 0) {
-            // select random uid from array for this row
-            let uids = document.scores[rowNumber - 1].uids;
-            let array_index = Math.floor(Math.random() * uids.length);
-            let uid = uids[array_index];
-            let image_id = "user_" + uid;
-            let seconds = 0;
 
-            switch (remainder) {
+          const cellwidth = 200 * uiScale;
+          const cellheight = 64 * uiScale;
+          const columnCount = 5;
+          const rowCount = document.scores.length + 1;
+
+          const cellsCount = columnCount * rowCount;
+          const gridwidth = columnCount * cellwidth;
+          const gridheight = rowCount * cellheight;
+
+          document.graphics = this.graphics;
+
+          document.wideCells = [];
+
+          var newCellObject = function (scene, cell) {
+            let cellText = "";
+            let bgColor;
+            let odd = false;
+            let firstCellofRow = 0;
+
+            // determine first cell of row, then determine if its an odd or even row based on that
+            const remainder = cell.index % columnCount;
+            if (remainder === 0) {
+              firstCellofRow = cell.index;
+            } else {
+              firstCellofRow = cell.index - remainder;
+            }
+            const rowNumber = firstCellofRow / columnCount;
+            if (rowNumber % 2 === 0) {
+              // odd
+              odd = true;
+            }
+
+            DEBUG && console.log("row number:");
+            console.log(rowNumber);
+
+            // determine if this is the head row
+            if (cell.index < columnCount) {
+              bgColor = Phaser.Display.Color.HexStringToColor("#002171").color;
+            } else if (odd) {
+              bgColor = Phaser.Display.Color.HexStringToColor("#0d47a1").color;
+            } else {
+              bgColor = Phaser.Display.Color.HexStringToColor("#2962ff").color;
+            }
+
+            var cellBg = scene.add
+              .graphics(0, 0)
+              .fillStyle(bgColor)
+              .fillRect(0, 0, 200 * uiScale, 64 * uiScale);
+
+            switch (cell.index) {
               case 0:
-                cellText =
-                  document.scores[rowNumber - 1].ranks[
-                    document.highScoreRegion
-                  ];
+                cellText = "Position";
                 break;
               case 1:
-                DEBUG && console.log("adding image from asset id " + image_id);
-                image = scene.add.image(
-                  (200 / 2) * uiScale,
-                  (64 / 2) * uiScale,
-                  image_id
-                );
-                image.displayWidth = cell.height;
-                image.displayHeight = cell.height;
+                cellText = "Avatar";
                 break;
               case 2:
-                cellText =
-                  document.scores[rowNumber - 1].nicknames[array_index];
+                cellText = "Nickname";
                 document.wideCells.push(cell.index);
                 break;
               case 3:
-                seconds = document.scores[rowNumber - 1].data.seconds;
-                if (seconds > 60) {
-                  minutes = Math.floor(seconds / 60);
-                  if (minutes < 10) {
-                    celltext =
-                      "0" + minutes + ":" + (seconds > 10 ? "0" : "") + seconds;
-                  } else {
-                    celltext =
-                      minutes + ":" + (seconds > 10 ? "0" : "") + seconds;
-                  }
-                } else {
-                  cellText = seconds;
-                }
+                cellText = "Time";
                 break;
               case 4:
-                cellText = document.scores[rowNumber - 1].score;
+                cellText = "Score";
+                break;
+              default:
+                cellText = cell.index;
                 break;
             }
-          }
 
-          var container;
-          DEBUG && console.log();
-          if (image !== null) {
-            DEBUG && console.log("we should be adding the image");
-            container = scene.add.container(0, 0, [cellBg, image]);
-          } else {
-            var txt = scene.add
-              .text((200 / 2) * uiScale, (64 / 2) * uiScale, cellText, {
-                fontSize: 35 * uiScale + "px",
-                fontFamily: "Luckiest Guy",
-              })
-              .setOrigin(0.5, 0.5);
-            container = scene.add.container(0, 0, [cellBg, txt]);
-          }
+            var image = null;
+            // dont do this in head row
+            if (rowNumber > 0) {
+              // select random uid from array for this row
+              let uids = document.scores[rowNumber - 1].uids;
+              let array_index = Math.floor(Math.random() * uids.length);
+              let uid = uids[array_index];
+              let image_id = "user_" + uid;
+              let seconds = 0;
 
-          return container;
-        };
+              switch (remainder) {
+                case 0:
+                  cellText =
+                    document.scores[rowNumber - 1].ranks[
+                      document.highScoreRegion
+                    ];
+                  break;
+                case 1:
+                  DEBUG &&
+                    console.log("adding image from asset id " + image_id);
+                  image = scene.add.image(
+                    (200 / 2) * uiScale,
+                    (64 / 2) * uiScale,
+                    image_id
+                  );
+                  image.displayWidth = cell.height;
+                  image.displayHeight = cell.height;
+                  break;
+                case 2:
+                  cellText =
+                    document.scores[rowNumber - 1].nicknames[array_index];
+                  document.wideCells.push(cell.index);
+                  break;
+                case 3:
+                  seconds = document.scores[rowNumber - 1].data.seconds;
+                  if (seconds > 60) {
+                    minutes = Math.floor(seconds / 60);
+                    if (minutes < 10) {
+                      celltext =
+                        "0" +
+                        minutes +
+                        ":" +
+                        (seconds > 10 ? "0" : "") +
+                        seconds;
+                    } else {
+                      celltext =
+                        minutes + ":" + (seconds > 10 ? "0" : "") + seconds;
+                    }
+                  } else {
+                    cellText = seconds;
+                  }
+                  break;
+                case 4:
+                  cellText = document.scores[rowNumber - 1].score;
+                  break;
+              }
+            }
 
-        var onCellVisible = function (cell) {
-          cell.setContainer(newCellObject(this, cell));
-          //console.log('Cell ' + cellIdx + ' visible');
-        };
+            var container;
+            DEBUG && console.log();
+            if (image !== null) {
+              DEBUG && console.log("we should be adding the image");
+              container = scene.add.container(0, 0, [cellBg, image]);
+            } else {
+              var txt = scene.add
+                .text((200 / 2) * uiScale, (64 / 2) * uiScale, cellText, {
+                  fontSize: 35 * uiScale + "px",
+                  fontFamily: "Luckiest Guy",
+                })
+                .setOrigin(0.5, 0.5);
+              container = scene.add.container(0, 0, [cellBg, txt]);
+            }
 
-        var table = this.add.rexGridTable(
-          this.getCenterX(),
-          this.getCenterY(),
-          gridwidth,
-          gridheight,
-          {
-            cellHeight: cellheight,
-            cellWidth: cellwidth,
-            cellsCount: cellsCount,
-            columns: columnCount,
-            cellVisibleCallback: onCellVisible.bind(this),
-          }
-        );
-        document.scores = false;
-        this.scoreBuildingInProgress = false;
-      });
-      loader.start();
+            return container;
+          };
+
+          var onCellVisible = function (cell) {
+            cell.setContainer(newCellObject(this, cell));
+            //console.log('Cell ' + cellIdx + ' visible');
+          };
+
+          this.table = this.add.rexGridTable(
+            this.getCenterX(),
+            this.getCenterY(),
+            gridwidth,
+            gridheight,
+            {
+              cellHeight: cellheight,
+              cellWidth: cellwidth,
+              cellsCount: cellsCount,
+              columns: columnCount,
+              cellVisibleCallback: onCellVisible.bind(this),
+            }
+          );
+          document.scores = false;
+          this.scoreBuildingInProgress = false;
+        });
+        loader.start();
+        this.postFxPlugin;
+      }
     }
   }
 
@@ -273,7 +319,7 @@ export default class HighScore extends FindItScene {
       .setOrigin(0.5, 0.5);
 
     this.regionText = this.add
-      .text(this.getCenterX(), 200 * uiScale, "", {
+      .text(this.getCenterX(), 250 * uiScale, "", {
         fontSize: 45 * uiScale + "px",
         align: "center",
         color: "#ffffff",
