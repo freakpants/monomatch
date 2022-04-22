8c4bf4a2a7b7d1e821ffe6ee1a94a8416c96604d
freakpants <freakpants@gmail.com>
2022-04-22 17:47:15 +0200

    even more performance attempts

--- scenes/Guessing.js
+++ scenes/Guessing.js
@@ -71,7 +71,7 @@ export default class Guessing extends FindItScene {
     var col, row;
     for (col = 0; col < 4; col++) {
       for (row = 0; row < 2; row++) {
-        this.assets[i] = this.add.image(
+        this.assets[i] = this.add.sprite(
           0,
           0,
           "asset" + document.set[0].icons[i]
